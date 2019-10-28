63f154d52bd7a45bb68360927b755bccf7ed1e90
Paunov, Andrey <Andrey.Paunov@openlane.com>
2019-10-28 18:54:03 -0400

    Initial commit

---
+++ app/src/main/java/com/andreypaunov/twitterquery/CustomViewModel.kt
@@ -0,0 +1,6 @@
+package com.andreypaunov.twitterquery
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class CustomViewModel(application: Application) : AndroidViewModel(application)
