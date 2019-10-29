a39b897b2886ddb1f720ba95b25442d719bf89af
Paunov, Andrey <Andrey.Paunov@openlane.com>
2019-10-29 01:15:18 -0400

    Successful Twiter login

--- app/src/main/java/com/andreypaunov/twitterquery/CustomViewModel.kt
+++ app/src/main/java/com/andreypaunov/twitterquery/CustomViewModel.kt
@@ -1,6 +1,18 @@
 package com.andreypaunov.twitterquery
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import com.twitter.sdk.android.core.Callback
+import com.twitter.sdk.android.core.Result
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.core.identity.TwitterLoginButton
 
-class CustomViewModel(application: Application) : AndroidViewModel(application)
+class CustomViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun onTwitterCallback(twitterLoginButton: TwitterLoginButton) {
+
+    }
+
+}
--- app/src/main/java/com/andreypaunov/twitterquery/MainActivity.kt
+++ app/src/main/java/com/andreypaunov/twitterquery/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.andreypaunov.twitterquery
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.andreypaunov.twitterquery.databinding.ActivityMainBinding
+import com.twitter.sdk.android.core.Callback
+import com.twitter.sdk.android.core.Result
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.core.identity.TwitterLoginButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var twitterLoginButton: TwitterLoginButton
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(CustomViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        twitterLoginButton = binding.twitterButtonId
+        twitterLoginButton.callback = object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>?) {
+                if (result != null) {
+                    Log.d("====", result.data.userName)
+                }
+            }
+
+            override fun failure(exception: TwitterException?) {
+                if (exception?.message != null) {
+                    Log.d("====", exception.message.toString())
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.d("====", "onActivityResult")
+
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data)
     }
 }
